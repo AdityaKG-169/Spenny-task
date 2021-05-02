f5f3b817f9c3e110506018f02962670dc4252d16
AdityaKG-169 <aditya.kg169@gmail.com>
2021-05-02 15:50:06 +0530

    Enforced https

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import HomePage from "./Pages/HomePage/HomePage";
 
 function App() {
+	useEffect(() => {
+		if (process.env.NODE_ENV !== "development") {
+			if (window.location.protocol === "http:") {
+				let link = window.location.href;
+				link = link.replace("http", "https");
+				window.location.href = link;
+			}
+		}
+	}, []);
+
 	return (
 		<>
 			<HomePage />
